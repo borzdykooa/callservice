46dce8a9153bb2a3b3847446bb24b97999104793
Olga Borzdyko <v.barzdyka@andersenlab.com>
2019-10-21 16:46:24 +0300

    Initial commit (fix)

--- src/main/java/com/borzdykooa/callservice/service/CallService.java
+++ src/main/java/com/borzdykooa/callservice/service/CallService.java
@@ -1,43 +1,45 @@
 package com.borzdykooa.callservice.service;
 
+import com.borzdykooa.callservice.dto.CallDto;
+import com.borzdykooa.callservice.exception.ApplicationException;
+import org.springframework.stereotype.Service;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
-import org.springframework.stereotype.Service;
-
-import com.borzdykooa.callservice.dto.CallDto;
-import com.borzdykooa.callservice.exception.ApplicationException;
-
 @Service
 public class CallService {
 
-	public void saveCall(CallDto callDto) {
-		String fileName = callDto.getFirstName() != null
-				? callDto.getLastName() + "_" + callDto.getFirstName() + ".txt" : callDto.getLastName() + ".txt";
-		File file = new File(String.join(File.separator, "calls", fileName));
-		try {
-			file.createNewFile();
-			try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
-				bufferedWriter.append(String.valueOf(callDto.getLocalDateTime().format(DateTimeFormatter.ofPattern("HH:mm:ss"))))
-						.append(" ")
-						.append(normalizePhoneNumber(callDto.getPhoneNumber()))
-						.append(System.lineSeparator());
-			}
-		} catch (IOException e) {
-			throw new ApplicationException("IOException occurred in CallService:saveCall");
-		}
-	}
+    private static final String DEFAULT_CODE_PART = "00420";
+    private static final String PHONE_NUMBER_REGEXP = "(\\d{5})(\\d{3})(\\d{3})(\\d{3})";
 
-	private String normalizePhoneNumber(String phoneNumber) {
-		phoneNumber = phoneNumber.replace("+", "00");
-		phoneNumber = phoneNumber.replaceAll("[\\-() ]", "");
-		if (phoneNumber.length() == 9) {
-			phoneNumber = "00420" + phoneNumber;
-		}
+    public void saveCall(CallDto callDto) {
+        String fileName = String.format("%s.txt", callDto.getFirstName() != null
+                ? callDto.getLastName() + "_" + callDto.getFirstName() : callDto.getLastName());
+        File file = new File(String.join(File.separator, "calls", fileName));
+        try {
+            file.createNewFile();
+            try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, true))) {
+                bufferedWriter.append(String.valueOf(callDto.getLocalDateTime().format(DateTimeFormatter.ofPattern("HH:mm:ss"))))
+                        .append(" ")
+                        .append(normalizePhoneNumber(callDto.getPhoneNumber()))
+                        .append(System.lineSeparator());
+            }
+        } catch (IOException e) {
+            throw new ApplicationException("IOException occurred in CallService:saveCall");
+        }
+    }
 
-		return phoneNumber.replaceAll("(\\d{5})(\\d{3})(\\d{3})(\\d{3})", "($1 $2 $3 $4)");
-	}
+    private String normalizePhoneNumber(String phoneNumber) {
+        phoneNumber = phoneNumber.replace("+", "00");
+        phoneNumber = phoneNumber.replaceAll("[\\-() ]", "");
+        if (phoneNumber.length() == 9) {
+            phoneNumber = DEFAULT_CODE_PART + phoneNumber;
+        }
+
+        return phoneNumber.replaceAll(PHONE_NUMBER_REGEXP, "($1 $2 $3 $4)");
+    }
 }
--- src/main/java/com/borzdykooa/callservice/validation/PhoneNumberConstraintValidator.java
+++ src/main/java/com/borzdykooa/callservice/validation/PhoneNumberConstraintValidator.java
@@ -10,41 +10,41 @@ import org.springframework.util.StringUtils;
 
 public class PhoneNumberConstraintValidator implements ConstraintValidator<PhoneNumberConstraint, String> {
 
-	private static final String PHONE_PATTERN = "^[+]?[0-9\\-() ]*$";
-	private static final String NON_DIGITAL_SYMBOLS = "[\\-() ]";
-	private static final int NUMBER_OF_ALL_DIGITS = 14;
-	private static final int NUMBER_OF_LOCAL_DIGITS = 9;
+    private static final String PHONE_PATTERN = "^[+]?[0-9\\-() ]*$";
+    private static final String NON_DIGITAL_SYMBOLS = "[\\-() ]";
+    private static final int NUMBER_OF_ALL_DIGITS = 14;
+    private static final int NUMBER_OF_LOCAL_DIGITS = 9;
 
-	public void initialize(PhoneNumberConstraint constraint) {
-	}
+    public void initialize(PhoneNumberConstraint constraint) {
+    }
 
-	public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
-		if (phoneNumber == null) {
-			return false;
-		}
-		Pattern pattern = Pattern.compile(PHONE_PATTERN);
-		Matcher matcher = pattern.matcher(phoneNumber);
-		if (!matcher.find() || wrongBrackets(phoneNumber)) {
-			return false;
-		}
-		phoneNumber = phoneNumber.replace("+", "00");
-		phoneNumber = phoneNumber.replaceAll(NON_DIGITAL_SYMBOLS, "");
-		if (numberHasWrongNumberOfDigts(phoneNumber) || numberHasWrongCode(phoneNumber)) {
-			return false;
-		}
+    public boolean isValid(String phoneNumber, ConstraintValidatorContext context) {
+        if (phoneNumber == null) {
+            return false;
+        }
+        Pattern pattern = Pattern.compile(PHONE_PATTERN);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        if (!matcher.find() || hasWrongBrackets(phoneNumber)) {
+            return false;
+        }
+        phoneNumber = phoneNumber.replace("+", "00");
+        phoneNumber = phoneNumber.replaceAll(NON_DIGITAL_SYMBOLS, "");
+        if (hasWrongNumberOfDigits(phoneNumber) || hasWrongCode(phoneNumber)) {
+            return false;
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	private boolean numberHasWrongCode(String phoneNumber) {
-		return phoneNumber.length() == NUMBER_OF_ALL_DIGITS && !phoneNumber.substring(0, 2).equals("00");
-	}
+    private boolean hasWrongCode(String phoneNumber) {
+        return phoneNumber.length() == NUMBER_OF_ALL_DIGITS && !phoneNumber.substring(0, 2).equals("00");
+    }
 
-	private boolean numberHasWrongNumberOfDigts(String phoneNumber) {
-		return phoneNumber.length() != NUMBER_OF_ALL_DIGITS && phoneNumber.length() != NUMBER_OF_LOCAL_DIGITS;
-	}
+    private boolean hasWrongNumberOfDigits(String phoneNumber) {
+        return phoneNumber.length() != NUMBER_OF_ALL_DIGITS && phoneNumber.length() != NUMBER_OF_LOCAL_DIGITS;
+    }
 
-	private boolean wrongBrackets(String phoneNumber) {
-		return StringUtils.countOccurrencesOf(phoneNumber, "(") != StringUtils.countOccurrencesOf(phoneNumber, ")");
-	}
+    private boolean hasWrongBrackets(String phoneNumber) {
+        return StringUtils.countOccurrencesOf(phoneNumber, "(") != StringUtils.countOccurrencesOf(phoneNumber, ")");
+    }
 }
